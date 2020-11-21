ad1c3e10e1f9a59d0ea882315fb32379ef108816
Ondre455 <74813204+Ondre455@users.noreply.github.com>
2020-11-21 12:53:07 +0400

    Add files via upload

---
+++ UCar/Data/Interfaces/IAllCars.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UCar.Data.Models;
+
+namespace UCar.Data.Interfaces
+{
+    public interface IAllCars
+    {
+        IEnumerable<Car> Cars { get; set; }
+        Car GetObjectCar(int CarID);
+    }
+}
---
+++ UCar/Data/Mocks/MockCathegory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UCar.Data.Interfaces;
+using UCar.Data.Models;
+
+namespace UCar.Data.Mocks
+{
+    public class MockCathegory : ICarCathegory
+    {
+        public IEnumerable<Cathegory> AllCathegories
+        {
+            get
+            {
+                return new List<Cathegory> { new Cathegory { CathegoryName = "Krossover" } };
+            }
+        }
+    }
+}
